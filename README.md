a91059d1773241a83bc2f39d62a058465876015f
Gatsbeau <jabriel0ne@gmail.com>
2023-09-26 00:08:39 -0400

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# w5c-calendar
+# Week 5 Challenge - Calendar
+
+## Description
+
+This week's assignment was another refactor, this time the goal was to make a timesheet that allowed users to record planned tasks in an 8-hour work day. This way, even if an individual refreshes or leaves the page, their recorded tasks will remain in their intended spots. The application should also inform the user of what hour they are currently on.
+
+This was a pretty simple project, especially considering the previous weeks. It served as a nice intro to interacting with Bootstrap elements as well as communicating with jQuery. The languages of both haven't quite settled in with me yet (this was a cursory task after all), but it's at least a simple way to ease into them.
+
+## Screenshot
+
+![Screencap of application](image.png)
+
+## Link to Deployed Application
+
+https://chasinggatsby.github.io/w5c-calendar/
--- script.js
+++ script.js
@@ -2,12 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   let timeBlock = $(".time-block");
   let toDo = $(".description");
 
@@ -18,11 +12,11 @@ $(function () {
       localStorage.setItem(key, taskText);
     });
   });
-  //
 
   timeBlock.each(function (i) {
     let currentHour = dayjs().format("HH");
     let id = timeBlock[i].id;
+    // separates the hour in the id and converts it into an integer to be compared to currentHour
     let hour = parseInt(id.split("-")[1]);
     if (currentHour == hour) {
       $(this).addClass("present");
@@ -33,18 +27,16 @@ $(function () {
     }
   });
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
   timeBlock.each(function (i) {
     let key = timeBlock[i].id;
+    // ensures that if text boxes
     if (localStorage.getItem(key) === undefined) {
       toDo.eq(i).text("");
     } else {
       toDo.eq(i).text(localStorage.getItem(key));
     }
   });
-  // TODO: Add code to display the current date in the header of the page.
+
   let date = $("#currentDay");
   date.text(dayjs().format("MMMM D, YYYY"));
 });
